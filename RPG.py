ba2a0e26d47a4562dcd7f91c3877950f6f3def11
David Patrick <95433031+OOOSecurityCorp@users.noreply.github.com>
2022-05-31 13:48:42 +0100

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -8,6 +8,7 @@ os.system("clear")
 def passwd():
 	#colors
 	yellow =  '\033[93m'
+	white = '\033[0m'
 	green = '\033[92m'
 	cyan = '\033[96m'
 	pink ='\033[95m'
@@ -22,31 +23,43 @@ def passwd():
 	t.sleep(0.5)
 	print (green + bold +"[coded by  000SecurityCorp]" + bold + green)
 	t.sleep(0.5)
-	print (yellow + bold + "==> github: https://www.github.com/000SecurityCorp.git <==" +bold +yellow)
+	print (yellow + bold + "[github: https://www.github.com/OOOSecurityCorp.git ]" +bold +yellow)
 	print (" ")
 	try :
 			t.sleep(0.5)
 			length = int(input(cyan+ bold+  "Enter the length of the password :   "+ bold + cyan))
-			print (yellow + bold + ":::::>password generator<:::::" + bold + yellow)
-			print ("  ")
-			t.sleep(0.7)
-			chara = "abcdefghijklmnopqrstuvwxyz0123456789@#$&%^*"
-			password = (green + bold + " " + bold + green)
-			for x in range(length):
-				password += random.choice(chara)
-				
-			t.sleep(1.0)
-			print (password)
-			print ("  ")
-			t.sleep(0.6)
-			print (yellow +bold + "==> here is the password you requested for <==" +bold +yellow)
-			print ("  ")
-			t.sleep(1.5)
-			print (green + bold +"*****THANK YOU FOR USING ME*****"+bold + green)
-			
 	except :
 			t.sleep(0.7)
 			print (red +"invalid input!")
+			passwd()
+	print (yellow + bold + "[-] GENERATING PASSWORD" + bold + yellow)
+	print ("  ")
+	t.sleep(0.7)
+	chara = "abcdefghijklmnopqrstuvwxyz0123456789@#$&%*"
+	password = (green + bold + " " + bold + green)
+	for x in range(length):
+		password += random.choice(chara)
+				
+	t.sleep(1.0)
+	print ("[+] PASSWORD: "+white+ password)
+	print ("  ")
+	t.sleep(0.6)
+	#print (yellow +bold + "==> here is the password you requested for <==" +bold +yellow)
+	print ("  ")
+	t.sleep(1.5)
+	print (green + bold +"*****THANK YOU FOR USING ME*****"+bold + green)
+	con = input("[#] Do You Want To Continue(y/n)?? ")
+	if con == "y" or con == "Y":
+		passwd()
+	elif con == "n" or con == "N":
+		print ("[~] Aborting...")
+		t.sleep(0.9)
+		exit(1)
+	else:
+		print ('[!] Wrong Input!!!')
+		t.sleep(0.2)
+		print ("[×] Abouting Script....")
+		t.sleep(0.9)
 			
 if __name__ == "__main__":
 	passwd()
